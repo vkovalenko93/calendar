1b34e8c734bad745d8829dc8ad847ce29a28a617
vkovalenko93 <oxx01092014@gmail.com>
2022-04-29 23:25:52 +0300

    deleted projects (WebApi, DB)

---
+++ Calendar.UI/Components/ClockComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.UI.Components
+{
+    public partial class ClockComponent
+    {
+
+        public string HourPoint { get; private set; }
+        public string MinutePoint { get; private set; }
+        public string SecondPoint { get; private set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            HourPoint = $"{DateTime.Now.Hour % 12 * 30 + DateTime.Now.Minute / 2}";
+            MinutePoint = $"{DateTime.Now.Minute * 6 }";
+            SecondPoint = $"{DateTime.Now.Second * 6}";
+        }
+    }
+}
